e2bd980ece2adc9ff6f9caa122887c8413bb5495
Denys Pavlyuk <densf2@gmail.com>
2024-01-28 13:34:59 +0200

    structured_with_describe

--- tests/users.test.ts
+++ tests/users.test.ts
@@ -6,37 +6,38 @@ import { writeFile } from 'node:fs/promises'
 let uname: String
 let pass: String
 let auth_token: String
-
-test('get list of users', async () => {
-    const response = await axios.get('https://dummyjson.com/users', {
-        headers: {
-            'authority': 'dummyjson.com',
-            'referer': 'https://dummyjson.com/docs/auth',
-        }
+describe('getting token', () => {
+    test('get list of users', async () => {
+        const response = await axios.get('https://dummyjson.com/users', {
+            headers: {
+                'authority': 'dummyjson.com',
+                'referer': 'https://dummyjson.com/docs/auth',
+            }
+        });
+        uname = String(jsonpath.query(response.data, '$.users[25].username'))
+        pass = String(jsonpath.query(response.data, '$.users[25].password'))
+        // console.log(uname)
+        // console.log(pass)
+        //await writeFile('response.json', JSON.stringify(response.data), 'utf8', (err) => {
+        //    if (err) throw err;
+        //    console.log('The file has been saved!')})
     });
-    uname = String(jsonpath.query(response.data, '$.users[25].username'))
-    pass = String(jsonpath.query(response.data, '$.users[25].password'))
-    // console.log(uname)
-    // console.log(pass)
-    //await writeFile('response.json', JSON.stringify(response.data), 'utf8', (err) => {
-    //    if (err) throw err;
-    //    console.log('The file has been saved!')})
-});
 
-test('get token by user credetials', async () => {
-    const token_response = await axios.post('https://dummyjson.com/auth/login',
-        {
-            'username': uname,
-            'password': pass,
-        },
-        {
-            headers: { 'Content-Type': 'application/json' }
-        })
-    auth_token = String(jsonpath.query(token_response.data, '$..token'))
-    try {
-        await writeFile('token2.json', JSON.stringify(auth_token))
-        console.log('The token has been saved!')
-    } catch (err: any) {
-        console.error('issue with saving token', err.message)
-    }
+    test('get token by user credetials', async () => {
+        const token_response = await axios.post('https://dummyjson.com/auth/login',
+            {
+                'username': uname,
+                'password': pass,
+            },
+            {
+                headers: { 'Content-Type': 'application/json' }
+            })
+        auth_token = String(jsonpath.query(token_response.data, '$..token'))
+        try {
+            await writeFile('token2.json', JSON.stringify(auth_token))
+            console.log('The token has been saved!')
+        } catch (err: any) {
+            console.error('issue with saving token', err.message)
+        }
+    })
 })
