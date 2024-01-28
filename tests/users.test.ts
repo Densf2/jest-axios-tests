1e743b82013b50bf78a3c58aa5d058e40cce21a7
Denys Pavlyuk <densf2@gmail.com>
2024-01-28 14:18:40 +0200

    added_test_for_get_by_id

--- tests/users.test.ts
+++ tests/users.test.ts
@@ -54,6 +54,11 @@ describe('dealing with products', () => {
     })
 
     test('get product by id', async() => {
-
+        let fNumber = faker.number.int({max: 20})
+        const get_product_by_id = await axios.get(`https://dummyjson.com/products/${fNumber}`,
+        {headers: {'Content-Type': 'application/json',
+                   'Authorization': `Bearer ${auth_token}` 
+        }})
+        expect(get_product_by_id.status).toEqual(200)
     })
 })
