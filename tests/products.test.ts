d8d4dfb93e490dbf114c6343cb18a8f10e22ba08
Denys Pavlyuk <densf2@gmail.com>
2024-01-30 22:07:47 +0200

    updated_tests_for_products_to_use_token

--- tests/products.test.ts
+++ tests/products.test.ts
@@ -1,10 +1,12 @@
 import { faker } from '@faker-js/faker';
+import axios from 'axios';
+import data from '../data/token2.json'
 
 describe('dealing with products', () => {
     test('get all products', async() => {
         const get_all_products = await axios.get('https://dummyjson.com/products',
         {headers: {'Content-Type': 'application/json',
-                   'Authorization': `Bearer ${auth_token}` 
+                   'Authorization': `Bearer ${data.token}` 
         }})
         expect(get_all_products.status).toEqual(200)
     })
@@ -13,7 +15,7 @@ describe('dealing with products', () => {
         let fNumber = faker.number.int({max: 20})
         const get_product_by_id = await axios.get(`https://dummyjson.com/products/${fNumber}`,
         {headers: {'Content-Type': 'application/json',
-                   'Authorization': `Bearer ${auth_token}` 
+                   'Authorization': `Bearer ${data.token}` 
         }})
         expect(get_product_by_id.status).toEqual(200)
     })
