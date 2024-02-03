11359043562337ddc92b9501ba153031fab3cc4b
Denys Pavlyuk <densf2@gmail.com>
2024-02-03 22:00:47 +0200

    added_test_for_all_categories

--- tests/categories.test.ts
+++ tests/categories.test.ts
@@ -1,20 +1,26 @@
 import axios from 'axios';
 import data from '../data/token2.json'
-import { writeFile } from 'node:fs/promises'
 
 describe('Categories', () => {
+    let arrCategories: string[] = [];
     test('get all products', async() => {
         const get_all_categories = await axios.get('https://dummyjson.com/products/categories',
         {headers: {'Content-Type': 'application/json',
                    'Authorization': `Bearer ${data.token}` 
         }})
         expect(get_all_categories.status).toEqual(200)
-        try {
-            await writeFile('data/categories.json', JSON.stringify(get_all_categories.data, null, 1))
-            console.log('All categories has been saved!')
-        } catch (err: any) {
-            console.error('issue with saving categories', err.message)
-        }
-        console.log(JSON.stringify(get_all_categories.data))
+        arrCategories = get_all_categories.data
     })
+
+    
+        // console.log(arrCategories)
+        test('get category by id', async() => {
+        // iterate all categories received in request above
+        for (let i =0; i < arrCategories.length; i++) {
+            let id = [i]
+            let category = arrCategories[i]
+            const get_specific_category = await axios.get(`https://dummyjson.com/products/category/${category}`)
+            expect(get_specific_category.status).toEqual(200)
+            }
+        }, 30000)
 })
