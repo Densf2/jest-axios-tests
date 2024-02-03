877322d1842be61454f1a70516983ac06fa56154
Denys Pavlyuk <densf2@gmail.com>
2024-02-03 13:49:06 +0200

    saved_categories_response_in_file

--- tests/categories.test.ts
+++ tests/categories.test.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import data from '../data/token2.json'
+import { writeFile } from 'node:fs/promises'
 
 describe('Categories', () => {
     test('get all products', async() => {
@@ -8,5 +9,12 @@ describe('Categories', () => {
                    'Authorization': `Bearer ${data.token}` 
         }})
         expect(get_all_categories.status).toEqual(200)
+        try {
+            await writeFile('data/categories.json', JSON.stringify(get_all_categories.data, null, 1))
+            console.log('All categories has been saved!')
+        } catch (err: any) {
+            console.error('issue with saving categories', err.message)
+        }
+        console.log(JSON.stringify(get_all_categories.data))
     })
 })
