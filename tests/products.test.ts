6d17570932effa995b37a2684c8542bdb8cb4f7e
Denys Pavlyuk <densf2@gmail.com>
2024-10-26 14:57:40 +0300

    added-eslint-esceptions_2

--- tests/products.test.ts
+++ tests/products.test.ts
@@ -1,6 +1,7 @@
-import { faker } from '@faker-js/faker';
-import axios from 'axios';
-import data from '../data/token2.json';
+/* eslint-disable @typescript-eslint/naming-convention */
+import { faker } from '@faker-js/faker'
+import axios from 'axios'
+import data from '../data/token2.json'
 
 describe('dealing with products', () => {
   test('get all products', async () => {
@@ -9,12 +10,12 @@ describe('dealing with products', () => {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${data.token}`,
       },
-    });
-    expect(get_all_products.status).toEqual(200);
-  });
+    })
+    expect(get_all_products.status).toEqual(200)
+  })
 
   test('get product by id', async () => {
-    let fNumber = faker.number.int({ max: 20 });
+    const fNumber = faker.number.int({ max: 20 })
     const get_product_by_id = await axios.get(
       `https://dummyjson.com/products/${fNumber}`,
       {
@@ -23,7 +24,7 @@ describe('dealing with products', () => {
           Authorization: `Bearer ${data.token}`,
         },
       },
-    );
-    expect(get_product_by_id.status).toEqual(200);
-  });
-});
+    )
+    expect(get_product_by_id.status).toEqual(200)
+  })
+})
