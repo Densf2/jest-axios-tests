74dc5f068b54e590a841577ffc62d9e5d3f5b4b5
Denys Pavlyuk <densf2@gmail.com>
2024-11-07 22:51:43 +0200

    moved-to-use-url-from-file

--- tests/check_creating.ts
+++ tests/check_creating.ts
@@ -1,5 +1,10 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/comma-dangle */
 // request with based on fetch api
-fetch('https://dummyjson.com/auth/login', {
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+import { AUTH_URL } from '../constants'
+
+fetch(`${AUTH_URL}`, {
   method: 'POST',
   headers: { 'Content-Type': 'application/json' },
   body: JSON.stringify({
@@ -8,5 +13,5 @@ fetch('https://dummyjson.com/auth/login', {
     // expiresInMins: 60, // optional
   }),
 })
-  .then((res) => res.json())
-  .then(console.log);
+  .then(async (res) => await res.json())
+  .then(console.log)
