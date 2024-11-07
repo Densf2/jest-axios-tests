ea9971fde7f630b4e4481b5577e77e118d3a09c4
Denys Pavlyuk <densf2@gmail.com>
2024-11-07 22:42:36 +0200

    moved-url-into-file

---
+++ constants.ts
@@ -0,0 +1,4 @@
+export const BASE_URL = 'https://dummyjson.com'
+export const AUTH_URL = `${BASE_URL}/auth/login`
+export const PRODUCTS_URL = `${BASE_URL}/products`
+export const USERS_URL = `${BASE_URL}/users`
